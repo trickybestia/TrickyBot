0a4d256302fa7644942de81c8a2e74124ed159df
TrickyBestia <trickybestia@gmail.com>
2021-01-26 20:20:44 +0300

    Add AssemblyInfo.cs

---
+++ TrickyBot/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly:AssemblyTitle("TrickyBot")]
+[assembly:AssemblyCompany("TrickyBot Team")]
+[assembly:AssemblyCopyright("Copyright © TrickyBot Team 2021")]
+[assembly:AssemblyVersion("0.1.0.0")]
