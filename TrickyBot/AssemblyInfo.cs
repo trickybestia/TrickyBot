cc40c1835e5a1b39403d280b2bcd13d3951126fe
TrickyBestia <kurtkurt999555@gmail.com>
2021-02-03 16:54:27 +0300

    Revert "Add AssemblyInfo.cs"
    This reverts commit 0a4d256302fa7644942de81c8a2e74124ed159df.

--- TrickyBot/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly:AssemblyTitle("TrickyBot")]
-[assembly:AssemblyCompany("TrickyBot Team")]
-[assembly:AssemblyCopyright("Copyright © TrickyBot Team 2021")]
-[assembly:AssemblyVersion("0.1.0.0")]
