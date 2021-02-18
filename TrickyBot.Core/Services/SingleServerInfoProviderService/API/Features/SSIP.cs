f6478792fa503e141e7717ab4caf98f3d9735cf8
TrickyBestia <trickybestia@gmail.com>
2021-02-18 07:38:09 +0300

    Add SingleServerInfoProviderService API

---
+++ TrickyBot.Core/Services/SingleServerInfoProviderService/API/Features/SSIP.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SSIP.cs" company="TrickyBot Team">
+// Copyright (c) TrickyBot Team. All rights reserved.
+// Licensed under the CC BY-ND 4.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Discord.WebSocket;
+
+namespace TrickyBot.Services.SingleServerInfoProviderService.API.Features
+{
+    // I think "SingleServerInfoProvider" is too long so "SSIP".
+    public static class SSIP
+    {
+        public static SocketGuild Guild => Bot.Instance.ServiceManager.GetService<SingleServerInfoProviderService>().Guild;
+    }
+}
