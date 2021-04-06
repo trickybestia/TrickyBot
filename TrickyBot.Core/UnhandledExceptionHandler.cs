517fefc5449370ca78f0009d2a553ce084cb2f59
TrickyBestia <trickybestia@gmail.com>
2021-04-06 14:12:08 +0300

    документирование UnhandledExceptionHandler

--- TrickyBot.Core/UnhandledExceptionHandler.cs
+++ TrickyBot.Core/UnhandledExceptionHandler.cs
@@ -11,17 +11,29 @@ using TrickyBot.API.Features;
 
 namespace TrickyBot
 {
+    /// <summary>
+    /// Обработчик необработанных исключений.
+    /// </summary>
     internal static class UnhandledExceptionHandler
     {
+        /// <summary>
+        /// Инициализирует статические поля класса <see cref="UnhandledExceptionHandler"/>.
+        /// </summary>
         static UnhandledExceptionHandler()
         {
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
         }
 
+        /// <summary>
+        /// Метод-заглушка, необходимый для вызова статического конструктора.
+        /// </summary>
         public static void Init()
         {
         }
 
+        /// <summary>
+        /// Обработчик события необработанного исключения.
+        /// </summary>
         private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Log.Error(typeof(UnhandledExceptionHandler), $"Необработанное исключение (terminating: {e.IsTerminating}):\n{e.ExceptionObject}");
